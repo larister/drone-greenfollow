2dd9e57638aabb21b89ac2bb4c6fd4bff5a1f129
Alastair Lockie <alastair.lockie@gmail.com>
2012-11-10 19:02:51 +0000

    Final commit did not work but fun anyway@

--- doSomething.js
+++ doSomething.js
@@ -3,22 +3,39 @@ var PNG = require('png-js');
 var fs = require('fs');
 var detect = require('./lib/detect');
 var client  = arDrone.createClient();
-var pixelAverage = require('./lib/pixel-average.js');
-var positionToDirection = require('./lib/position-to-direction.js');
+var pixelAverage = require('./lib/pixel-average');
+var positionToDirection = require('./lib/position-to-direction');
 
 // client.on('navdata', console.log);
 
 var width = 640;
 var height = 368;
 var pngStream = client.createPngStream();
+var first = true;
+
+function roundNumber(num, dec) {
+    var result = Math.round(num*Math.pow(10,dec))/Math.pow(10,dec);
+    return result;
+}
 
 pngStream.on('data', function(data){
     if(first) {
-        var pixels = detect(width, height, data, 4);
+        // fs.writeFile('output.png', data);
+    }
+    first = false;
+
+
+
+        var pixels = detect(width, height, data, 1);
+        // console.log(pixels.length);
+        if(pixels.length === 0){
+            console.log('nothing found');
+            return;
+        }
 		var average = pixelAverage(pixels);
 		var targetDirection = positionToDirection(average, width, height);
-		console.log(targetDirection);
-    }
+		// console.log(roundNumber(targetDirection[0], 2), roundNumber(targetDirection[1], 2));
+    // }
 });
 
 // client.takeoff();
